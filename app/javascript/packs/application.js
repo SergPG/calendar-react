b9dfa047684cc2091e5b12ea849c897c54ad880f
SergPG <serg-popov@ukr.net>
2023-01-12 00:50:23 +0200

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,11 @@
 // console.log('Hello World from Webpacker')
 
 import "../stylesheets/application.scss"
+// Support component names relative to this directory:
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+
+// Makes React dev tools etc, work.
+ReactRailsUJS.mountComponents();
+
+ReactRailsUJS.useContext(componentRequireContext);
